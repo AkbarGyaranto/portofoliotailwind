0161a0af6fd8b234f8b79d2bc94c51dfdd852c84
AkbarGyaranto <akbarxploit09@gmail.com>
2023-05-03 20:44:37 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['indeks.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#a855f7',
+        secondary: '#64748b',
+        dark: '#020617',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
+
